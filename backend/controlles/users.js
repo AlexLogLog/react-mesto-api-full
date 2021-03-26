a4bcd5a6e8879e69c02738532c9f31a510767265
unknown <alexvoy.voy@yandex.ru>
2021-03-26 20:41:52 +0300

    lr15

--- backend/controlles/users.js
+++ backend/controlles/users.js
@@ -22,7 +22,7 @@ module.exports.getUserMe = (req, res, next) => {
     .catch(next);
 };
 
-module.exports.newUser = (req, res, next) => {
+module.exports.newUser = async (req, res, next) => {
   const {
     name,
     about,
@@ -31,62 +31,63 @@ module.exports.newUser = (req, res, next) => {
     password,
   } = req.body;
 
-  bcrypt.hash(password, 10)
-    .then((hash) => {
-      UserSchema.create({
-        name,
-        about,
-        avatar,
-        email,
-        password: hash,
+  try {
+    const hash = await bcrypt.hash(password, 10);
+    const userNew = await UserSchema.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    })
+    return res.status(200).send({
+      name: userNew.name, about: userNew.about, avatar: userNew.avatar, email: userNew.email,
+    })
+  } catch (err) {
+    if (err.name === 'MongoError' && err.code === 11000) {
+      return next(new ConflictError('Пользователь с данным email уже есть'));
+    } else return next(err);
+  };
+};
+
+  module.exports.patchUserInfo = (req, res, next) => {
+    const { name, about } = req.body;
+    UserSchema.findByIdAndUpdate(
+      req.user._id,
+      { name, about },
+      { new: true, runValidators: true },
+    )
+      .orFail(new NotFoundError())
+      .then((user) => {
+        res.status(200).send(user);
       })
-        .then((user) => res.status(200).send(user))
-        .catch((err) => {
-          if (err.name === 'MongoError' && err.code === 11000) {
-            next(new ConflictError('Пользователь с данным email уже есть'));
-          } else next(err);
-        });
-    });
-};
+      .catch(next);
+  };
 
-module.exports.patchUserInfo = (req, res, next) => {
-  const { name, about } = req.body;
-  UserSchema.findByIdAndUpdate(
-    req.user._id,
-    { name, about },
-    { new: true, runValidators: true },
-  )
-    .orFail(new NotFoundError())
-    .then((user) => {
-      res.status(200).send(user);
-    })
-    .catch(next);
-};
+  module.exports.patchUserAvatar = (req, res, next) => {
+    const { avatar } = req.body;
+    UserSchema.findByIdAndUpdate(
+      req.user._id,
+      { avatar },
+      { new: true, runValidators: true },
+    )
+      .orFail(new NotFoundError())
+      .then((user) => {
+        res.status(200).send(user);
+      })
+      .catch(next);
+  };
 
-module.exports.patchUserAvatar = (req, res, next) => {
-  const { avatar } = req.body;
-  UserSchema.findByIdAndUpdate(
-    req.user._id,
-    { avatar },
-    { new: true, runValidators: true },
-  )
-    .orFail(new NotFoundError())
-    .then((user) => {
-      res.status(200).send(user);
-    })
-    .catch(next);
-};
-
-module.exports.login = (req, res, next) => {
-  const { email, password } = req.body;
-  UserSchema.findUserByCredentials(email, password)
-    .then((user) => {
-      const token = jwt.sign(
-        { _id: user._id },
-        JWT_SECRET,
-        { expiresIn: '7d' },
-      );
-      res.status(200).send({ token });
-    })
-    .catch(() => next(new UnauthorizedError('Неверный email или пароль')));
-};
+  module.exports.login = (req, res, next) => {
+    const { email, password } = req.body;
+    UserSchema.findUserByCredentials(email, password)
+      .then((user) => {
+        const token = jwt.sign(
+          { _id: user._id },
+          JWT_SECRET,
+          { expiresIn: '7d' },
+        );
+        res.status(200).send({ token });
+      })
+      .catch(() => next(new UnauthorizedError('Неверный email или пароль')));
+  };
